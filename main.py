68a39c6f1eb5a537047a0d4148bdb655321c98a9
TheHarshitDubey <secondmail781@gmail.com>
2026-02-11 17:32:16 +0530

    backend complete

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 from fastapi import FastAPI
 from pydantic import BaseModel
 import os
@@ -5,14 +6,17 @@ from dotenv import load_dotenv
 import google.generativeai as genai
 
 load_dotenv()
-
+print("ENV LOADED:", os.getenv("GEMINI_API_KEY"))
 genai.configure(api_key=os.getenv("Gemini_API_Key"))
-model=genai.GenerativeModel("gemini-pro")
+print("Configured Gemini")
+model=genai.GenerativeModel("models/gemini-flash-latest")
 
 
 app=FastAPI(title="Scam Detection API",
     description="Backend API that analyzes messages and detects potential scams using AI",
     version="1.0.0")
+
+
 
 @app.get("/")
 def root():
@@ -23,20 +27,33 @@ class chatRequest(BaseModel):
     message:str
 
 @app.post("/chat")
-def chat(request:chatRequest):
-   text= request.message.lower()
-   
-   prompt = f"""
+async def chat(request: chatRequest):
+
+    prompt = f"""
     You are a scam detection AI.
 
-    Analyze the following message and answer in this format ONLY:
-    is_scam: true or false
-    confidence: number between 0 and 1
-    reply: short explanation
+    Respond ONLY in valid JSON format like this:
+
+    {{
+      "is_scam": true or false,
+      "confidence": number between 0 and 1,
+      "reply": "short explanation"
+    }}
 
     Message:
-    {text}
+    {request.message}
     """
-   response=model.generate_content(prompt)
-   ai_text=response.text
-   return {"ai_response":ai_text}
+
+    try:
+        response = await model.generate_content_async(prompt)
+        ai_text = response.text.strip()
+
+        parsed_response = json.loads(ai_text)
+
+        return parsed_response
+
+    except Exception as e:
+        return {
+            "error": "AI processing failed",
+            "details": str(e)
+        }
